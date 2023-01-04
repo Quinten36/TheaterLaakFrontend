9913bbbd0c6da564916cd47f6df47e1936211935
QuintenK <codestuff4all@gmail.com>
2023-01-04 22:19:50 +0100

    Fixed cors settings, Made fetches to the api, did all the functional logic except the calculation if there is still seats left for a day

---
+++ src/components/show/showBanner.js
@@ -0,0 +1,12 @@
+
+import './../../css/show.scss';
+
+function Show(props) {
+  return (
+    <figure className='showBanner'>
+      <img src={props.image} alt={props.alt} className='showBannerImage'/>
+    </figure>
+  );
+}
+
+export default Show;
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,33 +1,71 @@
-
 import Accordion from 'react-bootstrap/Accordion';
+import ShowBanner from './../components/show/showBanner';
 import ShowSpeelItem from './../components/show/showSpeelItem';
 import './../css/show.scss';
+import { useLocation } from 'react-router';
+import React, { useState, useEffect } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import ShowInfoBanner from './../components/show/showInfoBanner';
 
-function Show() {
+function Show(props) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [program, setProgram] = useState([])
+  const [prices, setPrices] = useState([])
+
+  //Get showId from url params
+  const location = useLocation();
+  var parameter = location.pathname.split('/')[2];
+  
+  //do api call to fetch the show information
+  useEffect(() => {
+    fetch(`http://127.0.0.1:5086/api/show/`+parameter)
+      .then(response => response.json())
+      .then((usefulData) => {
+        //do another fetch to retrieve the program information of the show
+        fetch('http://127.0.0.1:5086/api/program/'+usefulData.programId)
+          .then(resp => resp.json())
+          .then((data) => {setProgram(data)} )
+        // setLoading(false);
+        //Set the prices in a different state because the fetch will be done multiple times and so we dont need to dig in all the objects to check if the prices exists
+        setPrices({'firstClassPrice':usefulData.firstClassPrice, 'secondClassPrice':usefulData.secondClassPrice, 'thirdClassPrice':usefulData.thirdClassPrice})
+        setData(usefulData);
+      })
+      .catch((e) => {
+        console.error(`An error occurred: ${e}`)
+      });
+  }, []);
+
+  //check of de data er is vanwege rerenders en fromat naar uur en min
+  if (data != null){
+    var timeTotal = Date.parse(data.end) - Date.parse(data.start);
+    var minutes = Math.floor((timeTotal / (1000 * 60)) % 60),
+    hours = Math.floor((timeTotal / (1000 * 60 * 60)) % 24);
+  }
+
+  /*** Basic fetch method *************************************/
+  //fetch show data
+  // useEffect(() => {
+  //   fetch('http://127.0.0.1:5086/api/show/'+showId)
+  //    .then(resp => resp.json())
+  //    .then((data) => setShow(data))
+  //   }, [])
+
   return (
     <main>
-      <figure className='showBanner'>
-        <img src='/pexels.jpg' alt='Plaatje' className='showBannerImage'/>
-      </figure>
+      <ShowBanner image='/pexels.jpg' alt='Plaatje'/>
       <div>
-        <section>
-          <h3 className='showGenre'>Genre</h3>
-          <h2 className='showTitel'>Aladdin</h2>
-          <div className='showArtistHolder'>
-            <span>Booooooooob</span>,<span>Peeeeeetuuuur</span>,<span>Guanz</span>
-          </div>
-        </section>
+        <ShowInfoBanner programId={program.id} groupId={program.groupId} title={program.title}/>
         <section className='showDetails'>
           <hr className='showDetailsDivider'/>
-            Duration: <span>1 uur 35 min</span>
+            Duration: <span>{hours} uur {minutes} min</span>
           <hr className='showDetailsDivider'/>
-          Zaal <span>4</span> <span>Vlierbloesem</span>
+          Zaal <span>{data != null && data.hallId}</span> <span>Vlierbloesem</span>
           <hr className='showDetailsDivider'/>
         </section>
         <article className='showDescriptionArticle'>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,se of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          <br/><br/>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, nd more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          {program != null && program['description']}
         </article>
         <section className='showSpeelBanner'>
           <h3>Speel data</h3>
@@ -35,8 +73,6 @@ function Show() {
           <Accordion defaultActiveKey={['0', '1']} alwaysOpen className='speelData-accordion'>
             <Accordion.Item eventKey="0"> 
               <Accordion.Header><h2 className='speelDataHead'>December 2022<hr/></h2></Accordion.Header>
-              
-                {/* test */}
                 <Accordion.Body>
                   <ShowSpeelItem speelDatum='Vr 19 dec 20:20' beschikbaar={false}/>
                   <ShowSpeelItem speelDatum='Za 20 dec 19:35' beschikbaar={true}/>
@@ -57,19 +93,26 @@ function Show() {
           <h3>Prijzen</h3>
           <p>De volgende prijzen zijn de minimale prijs voor deze show. De dag kan invloed hebben op de prijs.</p>
           <section className='showPriceHolder'>
+            {/* check of the prijs er is, zoja, laat hem zien */}
+            {prices.firstClassPrice != null && 
             <div className='priceItem'>
-              <span className='priceRang'>1e rang</span><span className='price'>€45.00</span>
+              <span className='priceRang'>1e rang</span><span className='price'>€{prices.firstClassPrice.toFixed(2)}</span>
             </div>
+            }
+            {prices.secondClassPrice != null &&
             <div className='priceItem'>
-              <span className='priceRang'>2e rang</span><span className='price'>€38.00</span>
+              <span className='priceRang'>2e rang</span><span className='price'>€{prices.secondClassPrice.toFixed(2)}</span>
             </div>
+            }
+            {prices.thirdClassPrice != null &&
             <div className='priceItem'>
-              <span className='priceRang'>3e rang</span><span className='price'>€30.00</span>
+              <span className='priceRang'>3e rang</span><span className='price'>€{prices.thirdClassPrice.toFixed(2)}</span>
             </div>
+            }
           </section>
         </section>
         <section className='showRecommodetionsBanner'>
-          <button className='showAllShowBtn'>Alle voorstellingen</button>
+          <Nav.Link className='showAllShowBtn' href="/programmering">Alle voorstellingen</Nav.Link>
         </section>
       </div>
     </main>
