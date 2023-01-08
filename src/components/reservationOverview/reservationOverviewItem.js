5e837a22ed35e69eaaf20f6b2f88663c2e227501
QuintenK <codestuff4all@gmail.com>
2023-01-08 19:44:39 +0100

    Made reservation overview nice and added logic zo you can retrive reservations based on the dates you specified

---
+++ src/components/reservationOverview/reservationOverviewItem.js
@@ -0,0 +1,39 @@
+import './../../css/show.scss';
+import React, { useState, useEffect } from 'react';
+
+function Show(props) {
+  let [person, setPerson] = useState(null);
+  fetch('http://127.0.0.1:5086/api/account/name/'+props.gereserveerdDoor)
+      .then(resp => resp.json())
+      .then((data) => {console.log(data); setPerson(data.username)})
+  // console.log(respons)
+
+  return (
+    <div className='ROReserveringItem' key={props.keyId}>
+      <div className='ROHallNumberHolder'>
+          <span className='ROHallNumber'>{props.hallId}</span>{/*hall nummer*/}
+      </div>
+      <div className='RODateHolder'>
+          {/* only get the date and time and remove the milli seconds. Replace T with a space */}
+          <span>{props.start.split('.')[0].replace('T', ' ')}</span> tot <span>{props.end.split('.')[0].replace('T', ' ')}</span>{/*van tot datum en tijd*/}
+      </div>
+      {/* if statement of hij betaald heeft of niet */}
+      {props.hasPaid &&
+        <span className='ROPaid'>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+        <br/>Paid
+        </span>
+      }
+      {!props.hasPaid && 
+        <span className='ROPaid RONotPaid'>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>                    
+          <br/>Unpaid
+        </span>
+      }
+      {/* todo: later dit veranderen naar een clickable link */}
+      <div className='ROGereserveerde'>Gereserveerd door: <span id='ROGereserveerdeName'>{person}</span></div>
+    </div>
+  );
+}
+
+export default Show;
--- src/pages/ReserveringOverview.js
+++ src/pages/ReserveringOverview.js
@@ -1,22 +1,44 @@
 import './../css/reserveringOverview.scss';
 import React, { useState, useEffect } from 'react';
 import Nav from 'react-bootstrap/Nav';
+import ReservationOverviewItem from './../components/reservationOverview/reservationOverviewItem';
 
 function Show(props) {
-    // do state stuff
-    let [startDate, setStartDate] = useState(null);
-    let [endDate, setEndDate] = useState(null);
+  // do state stuff
+  let [startDate, setStartDate] = useState(null);
+  let [endDate, setEndDate] = useState(null);
+  let [reservationOverview, setReservationOverview] = useState(null);
+  var listItems;
 
-    //get currect date and the date a week later
-    var someDate = new Date();
-    var numberOfDaysToAdd = 7;
-    startDate = new Date(someDate).toISOString().split("T")[0];
-    var dateWeekLater = someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
-    endDate = new Date(dateWeekLater).toISOString().split("T")[0];
+  //get currect date and the date a week later
+  var someDate = new Date();
+  var numberOfDaysToAdd = 7;
+  startDate = new Date(someDate).toISOString().split("T")[0];
+  var dateWeekLater = someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
+  endDate = new Date(dateWeekLater).toISOString().split("T")[0];
 
-    function updateOverview() {
-        console.log('Called this')
-    }
+  function updateOverview() {
+    // get and set the new values of the inputs
+    startDate = document.getElementById('ROStart').value;
+    endDate = document.getElementById('ROEnd').value;
+
+    //format the date to format the api accept
+    var formatDate = startDate.split('-');
+    var reformatStart = formatDate[1] +'-'+ formatDate[2] +'-'+formatDate[0];
+    formatDate = endDate.split('-');
+    var reformatEnd = formatDate[1] +'-'+ formatDate[2] +'-'+formatDate[0];
+
+    //do api request
+    fetch('http://127.0.0.1:5086/api/reservation/filtered?start='+reformatStart+'&end='+reformatEnd)
+      .then(resp => resp.json())
+      .then((data) => {setReservationOverview(data); })
+  }
+
+  //do the standard search
+  useEffect(() => {
+    updateOverview();
+  }, [])
+  
 
   /*** Basic fetch method *************************************/
   //fetch show data
@@ -28,45 +50,33 @@ function Show(props) {
 
   return (
     <main>
-        <section className='ROSelectionBanner'>
-            {/* later meer filter opties toevoegen */}
-            <p className='ROSelectionMenu'>
-                {/* Gereserveerde zalen van <br/><span>02/01/2023</span> tot <span>08/02/2023</span> */}
-                Gereserveerde zalen van <br/>
-                <input type='date' name='ROStart' defaultValue={startDate} className='ROSelectDate'/> <span> </span>
-                tot <input type='date' name='ROEnd' defaultValue={endDate}/>
-                <button className='ROSelectionUpdateBtn' onClick={updateOverview}>Update</button>
-            </p>
-            <div className='ROLegenda'>
-                <span>
-                    Zaalnummer
-                </span>
-                <span>
-                    Datum
-                </span>
-                <span>
-                    Betaald?
-                </span>
-            </div>
-        </section>
-        <section className='ROFlexHolder'>
-            {/*deze meerdere malen repeaten*/}
-            <div className='ROReserveringItem'>
-                <div className='ROHallNumberHolder'>
-                    <span className='ROHallNumber'>1</span>{/*hall nummer*/}
-                </div>
-                <div>
-                    <span>03/01/2023 16:00</span> tot <span>03/01/2023 18:00</span>{/*van tot datum en tijd*/}
-                </div>
-                <span className='ROPaid'>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
-                    <br/>Paid
-                </span>{/*hij heeft betaald*/}
-                {/* <span className='ROPaid RONotPaid'>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>                    <br/>Unpaid
-                </span>hij heeft niet betaald */}
-            </div>
-        </section>
+      <section className='ROSelectionBanner'>
+        {/* later meer filter opties toevoegen */}
+        <p className='ROSelectionMenu'>
+          {/* Gereserveerde zalen van <br/><span>02/01/2023</span> tot <span>08/02/2023</span> */}
+          Gereserveerde zalen van <br/>
+          <input type='date' name='ROStart' id='ROStart' defaultValue={startDate} className='ROSelectDate'/> <span> </span>
+          tot <input type='date' name='ROEnd' id='ROEnd' defaultValue={endDate} className='ROSelectDate'/>
+          <button className='ROSelectionUpdateBtn' onClick={updateOverview}>Update</button>
+        </p>
+        <div className='ROLegenda'>
+          <span>Zaalnummer</span>
+          <span>Datum</span>
+          <span>Betaald?</span>
+        </div>
+      </section>
+      <section className='ROFlexHolder'>
+        {/*deze meerdere malen repeaten*/}
+        {reservationOverview == null || reservationOverview.length == 0 &&
+          <p className='RONoResult'>Geen reserveringen in dit zoekresultaat</p>
+        }
+        {reservationOverview != null &&
+          reservationOverview.map((item, index) => (
+            <ReservationOverviewItem key={item.id} keyId={item.id} hallId={item.hallId} start={item.start} end={item.end} hasPaid={item.hasPaid} gereserveerdDoor={item.accountId}/>
+            // <span key={item.id}>{artist.name}{index != reservationOverview.length-1 ? ', ' : ''}</span> 
+          ))
+        }
+      </section>
     </main>
   );
 }
