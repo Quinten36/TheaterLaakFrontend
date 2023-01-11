ca6890a728f43ace7685aed8ebc1d10720648351
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2023-01-11 10:09:28 +0100

    in between commit

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,53 +1,43 @@
-export default class Api {
-    constructor(url) {
-        this.url = url
-    };
+import env from "../../package.json";
+import data from "bootstrap/js/src/dom/data";
 
-    async get(query) {
-        return fetch(`${this.url}${query}`)
-            .then(response => response.json())
-    }
-
-    async post(query, data) {
-        return fetch(`${this.url}${query}`, {
-            headers: {
-                'X-CSRF-TOKEN': 'meta[name="csrf-token"]',
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify(data)
-        }).then(response => response.json())
-            .then(data => {
-                return data
-            })
-    }
-
-    async put(query, id, data) {
-        return fetch(`${this.url}${query}/${id}`, {
-            headers: {
-                'X-CSRF-TOKEN': 'meta[name="csrf-token"]',
-                'Content-Type': 'application/json',
-                'Accept' : 'application/json'
-            },
-            method: "PUT",
-            body: JSON.stringify(data)
-        }).then(response => response.json())
-            .then(data => {
-                return data
-            });
-
-    }
-
-    async delete(query, id) {
-        return fetch(`${this.url}${query}/${id}`, {
-            headers: {
-                'X-CSRF-TOKEN': 'meta[name="csrf-token"]',
-            },
-            method: "DELETE",
-        }).then(response => response.json)
-            .then(data => {
-                return data
-            })
-
-    }
+const url = `http://${env.ipadress}:${env.port}/api/`
+export async function get(query) {
+    return fetch(`${url}${query}`)
+        .then(response => response.json())
+        .then(data => data)
+        .catch(e => {
+            console.log(e);
+        })
 }
+async function post(query, data) {
+    return fetch(`${url}${query}`, {
+        headers: {
+            'X-CSRF-TOKEN': 'meta[name="csrf-token"]',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(data)
+    }).then(response => response.json())
+}
+async function put(query, id, data) {
+    return fetch(`${url}${query}/${id}`, {
+        headers: {
+            'X-CSRF-TOKEN': 'meta[name="csrf-token"]',
+            'Content-Type': 'application/json',
+            'Accept' : 'application/json'
+        },
+        method: "PUT",
+        body: JSON.stringify(data)
+    }).then(response => response.json())
+}
+export async function destroy(query, id) {
+    return fetch(`${url}${query}/${id}`, {
+        headers: {
+            'X-CSRF-TOKEN': 'meta[name="csrf-token"]',
+        },
+        method: "DELETE",
+    })
+
+}
+
--- src/Pages/Programming.js
+++ src/Pages/Programming.js
@@ -2,50 +2,30 @@ import SearchBar from "../Components/SearchBar";
 import SearchDropDown from "../Components/SearchDropDown";
 import {useEffect, useState} from "react";
 import ProgramCard from "../Components/ProgramCard";
-import env from '../../package.json';
-import Api from "../Api/Api";
-
+import * as ProgramApi from '../Api/Program';
 
 export default function Programming(props) {
     const [programs, setPrograms] = useState([]);
-    const api = new Api(`http://${env.ipadress}:${env.port}/api/`);
-    const array = ["John Bananeneter", "Harry Hanglip", "Rien aan de Lichtelaaie"]
+    const array = ["John Bananeneter", "Harry Hanglip", "Rien aan de Lichtelaaie"];
 
-    useEffect(() => {
-        fetch(`http://${env.ipadress}:${env.port}/api/Program`)
-            .then(response => response.json())
-            .then(data => {
-                setPrograms(data)
-                console.log(programs)
-            });
-    }, []);
+    useEffect( () => {
+        (async () =>
+            setPrograms(await ProgramApi.all())
+        )();
+    }, [setPrograms])
 
-    // useEffect(() => {
-    //     api.get("Program").then(
-    //         JSONprograms => {
-    //             console.log(JSONprograms)
-    //             setPrograms(JSONprograms)
-    //             // JSONprograms.map(program => programs.push({image: program.image, title: program.title, genres: [{name: "horror"}]}))
-    //             console.log(programs)
-    //         },
-    //         error => console.log(error)
-    //     )
-    // }, [])
-
-
+    console.log(programs);
 
     return (
         <div style={{display:'flex', alignItems:'center', flexDirection:'column', padding:'10px'}}>
             <SearchBar name="Datum" type="date" />
             <SearchBar name="Voorstelling of artiest"/>
             <SearchDropDown />
-            {programs.map(program => {
-                return <ProgramCard image={program.image} title={program.title} genre={program.genres} startDatum="22-8-2000" eindDatum="22-8-2001"></ProgramCard>
-            })}
+            {programs.map(program =>
+                <ProgramCard image={program.image} title={program.title} genre={program.genres} startDatum="22-8-2000" eindDatum="22-8-2001"></ProgramCard>
+            )}
             <ProgramCard image="alladin.png" title="Sinterklaas" genre="kinderen" startDatum="22-8-2000" eindDatum="22-8-2001" artiesten={array}  />
             <ProgramCard image="alladin.png" title="Sinterklaas" genre="kinderen" startDatum="22-8-2000" eindDatum="22-8-2001" artiesten={array}  />
         </div>
-
-
     )
 }
