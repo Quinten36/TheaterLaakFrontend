f99f1b05aebfdbd257f9c3c463f49b8052589fda
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2023-01-12 19:49:44 +0100

    make searchbar

--- src/Api/Program.js
+++ src/Api/Program.js
@@ -1,6 +1,15 @@
-import { get, destroy as des } from "./Api";
+import { destroy as des } from "./Api";
+import env from "../../package.json";
 
 const route = "Program";
 
-export const all = () => get(route);
+// export const all = () => get(route);
+
+const url = `http://${env.ipadress}:${env.port}/api/`
+const query = "Program"
+export const all = (search) => {
+    return fetch(url + query + '?' + search)
+        .then(response => response.json())
+        .catch(e => console.log(e))
+}
 export const destroy = (id) => des(route, id);
