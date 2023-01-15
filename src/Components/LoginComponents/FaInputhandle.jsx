c5dd0dcba261be2179d206cbefa96601255f8de0
Dtvanaanhout <94524307+Dtvanaanhout@users.noreply.github.com>
2023-01-15 12:38:45 +0100

    error handling validation

--- src/Components/LoginComponents/FaInputhandle.jsx
+++ src/Components/LoginComponents/FaInputhandle.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { Navigate } from 'react-router-dom';
+
 export default function FaInputhandle() {
   const [input1, setInput1] = useState('');
   const [input2, setInput2] = useState('');
@@ -10,75 +10,83 @@ export default function FaInputhandle() {
   const [input5, setInput5] = useState('');
 
   const gebruikerID = localStorage.getItem("id")
-  
-  
+  const [fouteInvoer, setFouteInvoer] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const inputToString = input1.toString() + input2.toString() + input3.toString() + input4.toString() + input5.toString();
     SendVerificatieCodeToBackEnd(inputToString);
-    
   }
 
-  async function SendVerificatieCodeToBackEnd(inputToString){
-    const response = await fetch(`https://localhost:7098/api/Registratie/api/validate/${gebruikerID}/${inputToString}`,{
+  async function SendVerificatieCodeToBackEnd(inputToString) {
+    const response = await fetch(`https://localhost:7098/api/Registratie/api/validate/${gebruikerID}/${inputToString}`, {
       method: 'Put',
-     })
-     
-     if(response.ok){
+    })
+
+    if (response.ok) {
       console.log("succes")
-     }
-     if (!response.ok) {
+    }
+    if (!response.ok) {
       const error = JSON.parse(await response.text());
-      console.log(error);
-     }  
+      setFouteInvoer( true)
+      
+    }    
   }
-  
+
   return (
     <div>
-    <Form className="FA_collectie"  >
-      <Form.Control
-        className="FA_box"
-        type="email"
-        placeholder="0"
-        maxLength="1"
-        value={input1}
-        onChange={(event) => setInput1(event.target.value)}
-      />
-      <Form.Control
-        className="FA_box"
-        type="email"
-        placeholder="0"
-        maxLength="1"
-        value={input2}
-        onChange={(event) => setInput2(event.target.value)}
-      />
-      <Form.Control
-        className="FA_box"
-        type="email"
-        placeholder="0"
-        maxLength="1"
-        value={input3}
-        onChange={(event) => setInput3(event.target.value)}
-      />
-      <Form.Control
-        className="FA_box"
-        type="email"
-        placeholder="0"
-        maxLength="1"
-        value={input4}
-        onChange={(event) => setInput4(event.target.value)}
-      />
-      <Form.Control
-        className="FA_box"
-        type="email"
-        placeholder="0"
-        maxLength="1"
-        value={input5}
-        onChange={(event) => setInput5(event.target.value)}
-      />
-  </Form>
-  <Button className="SubmitButton2FA" variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
-</div>
+      <Form className="FA_collectie">
       
-    
-)}
+        <Form.Control
+
+          className="FA_box"
+          type="text"
+          placeholder="0"
+          maxLength="1"
+          value={input1}
+          onChange={(event) => setInput1(event.target.value)}
+        />
+        <Form.Control
+
+          className="FA_box"
+          type="text"
+          placeholder="0"
+          maxLength="1"
+          value={input2}
+          onChange={(event) => setInput2(event.target.value)}
+        />
+        <Form.Control
+
+          className="FA_box"
+          type="text"
+          placeholder="0"
+          maxLength="1"
+          value={input3}
+          onChange={(event) => setInput3(event.target.value)}
+        />
+        <Form.Control
+
+          className="FA_box"
+          type="text"
+          placeholder="0"
+          maxLength="1"
+          value={input4}
+          onChange={(event) => setInput4(event.target.value)}
+        />
+        <Form.Control
+          
+          className="FA_box"
+          type="text"
+          placeholder="0"
+          maxLength="1"
+          value={input5}
+          onChange={(event) => setInput5(event.target.value)}
+        />
+      
+      
+      </Form >
+      {fouteInvoer ? <div className = "ErrorVerificatiePage" >De verificatie code is incorrect.</div> : null}
+      <Button className="SubmitButton2FA" variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+    </div>
+  )
+}
