e28f93f9309a6c320a7aee294d7f9b1168b4701c
Dtvanaanhout <94524307+Dtvanaanhout@users.noreply.github.com>
2023-01-18 14:55:53 +0100

    donateursfeedback

--- src/Components/LoginComponents/Login.jsx
+++ src/Components/LoginComponents/Login.jsx
@@ -91,9 +91,9 @@ export default function Login() {
 
   
   return (
-    <div className = "LoginContainer">
+    <div className = "LoginContainer" >
     <h1>Theather Laak</h1> 
-    <Form>
+    <Form >
             <Form.Control className = "LoginPageInputField" isInvalid={state.Username.usernameInputFOUT} onChange={handleUsernameInput} value={state.Username.usernameInput} type="email" placeholder="Gebruikersnaam" />
             <Form.Control className = "LoginPageInputField" isInvalid={state.Password.PasswordInputFOUT} onChange={handlePasswordInput} value={state.Password.PasswordInput}type="password" placeholder="Password" />
             <Form.Control.Feedback className="FeedbackOpInput" type="invalid">{state.Username.userNameError}</Form.Control.Feedback>
