3beded34aa7216b00a06e343fb974d49715ca328
Dtvanaanhout <94524307+Dtvanaanhout@users.noreply.github.com>
2023-01-19 12:20:44 +0100

    donateurs portaal changes
    refresh on submit

--- src/Components/DonateursportaalComponents/EarlyAccessReservations.jsx
+++ src/Components/DonateursportaalComponents/EarlyAccessReservations.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react'
 import { Nav } from 'react-bootstrap';
 export default function EarlyAccessReservations() {
   const [showsEarlyAccess, setShowsEarlyAccess] = useState()
-
+  
   async function fetchShows() {
     try {
       const response = await fetch('http://localhost:5086/api/Show', {
@@ -24,13 +24,13 @@ export default function EarlyAccessReservations() {
 
   }, []);
   return (
-    <div className='showsEarlyAccessKaart'>
+    <div className='Kaartcontainer'>
       {showsEarlyAccess && showsEarlyAccess.slice(0, 3).map((item, index) => (
-        <Card key={index}>
+        <Card className='kaartearlyaccess' key={index}>
           <Card.Img variant="top" src={item.program.image} />
           <Card.Body>
             <Card.Title>{item.program.title}</Card.Title>
-            <Card.Text>Kaarten vanaf €{item.firstClassPrice} * Rolstoel accesible/language </Card.Text>
+            <Card.Text>Kaarten vanaf €{item.firstClassPrice.toFixed(2)} * Rolstoel accesible/language </Card.Text>
             <Card.Text>{item.program.description}</Card.Text>
             <Nav.Link href={'/show/' + item.program.id}><Button variant="primary">Info & Kaarten</Button></Nav.Link>
           </Card.Body>
