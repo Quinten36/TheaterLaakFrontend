e6bb27f1385788760c77184a67477634710e494f
treinDotJS <82215214+treinDotJS@users.noreply.github.com>
2023-01-22 20:54:38 +0100

    Cleanup shoppingcartpage code

--- src/Pages/ShoppingCartPage.jsx
+++ src/Pages/ShoppingCartPage.jsx
@@ -3,15 +3,13 @@ import { Nav } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import ShoppingCart from "../Components/ShoppingCart/ShoppingCart.jsx";
 import "./../Css/shoppingCart.css";
-import {post} from "./../Api/Api"
 import ShoppingCartItems from "../Models/ShoppingCartItems.js";
 import { getCookie } from "../Cookie/Cookie.js";
 
 export default function ShoppingCartPage() {
   const [shoppingCartItems, setShoppingCartItems] = useState(getShoppingCartCookie());
   const [totalPrice, setTotalPrice] = useState(0)
-  console.log(shoppingCartItems)
-  console.log(totalPrice)
+
   function getShoppingCartCookie() {
       const cookie = getCookie("ShoppingCart");
       if(cookie !== "")
@@ -19,37 +17,9 @@ export default function ShoppingCartPage() {
       return new ShoppingCartItems();
   }
 
-  async function createTicketsList() {
-    shoppingCartItems.seatShowItems.forEach(async (seatShowItem) => {
-      await post("Ticket", {
-        "seatId": seatShowItem.seat.id,
-        "accountId": null,
-        "showId": seatShowItem.show.id,
-        "orderId": null
-      })
-      .then((response) => console.log(response))
-    })
-  }
-
-  async function createOrder() {
-    var ticketsList = await createTicketsList();
-
-    await post("Order", {
-      "hasPaid": false,
-      "accountId": null,
-      // ""
-    })
-  }
-
-  async function submitShoppingCart() {
-    await createOrder()
-    document.getElementById("shoppingCartForm").submit()
-  }
-
-
   return <main>
           <ShoppingCart className="shoppingCart" setTotalPrice={setTotalPrice} totalPrice={totalPrice} shoppingCartItems={shoppingCartItems}/>
-          <form id="shoppingCartForm" method="post" onSubmit={submitShoppingCart} action={"https://fakepay.azurewebsites.net"}  encType="application/x-www-form-urlencoded" className="bestelButtons">
+          <form id="shoppingCartForm" method="post" action={"https://fakepay.azurewebsites.net"}  encType="application/x-www-form-urlencoded" className="bestelButtons">
             <input name="amount" value={totalPrice.toFixed(2)} hidden readOnly></input>
             <input name="reference" value={1212121212} hidden readOnly></input>
             <input name="url" value={"http://localhost:5086/api/misc/setPayment"} hidden readOnly></input>
