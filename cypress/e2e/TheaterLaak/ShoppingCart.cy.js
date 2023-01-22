891172099ff34050bdc62cda79f6b6cc16360e7b
treinDotJS <82215214+treinDotJS@users.noreply.github.com>
2023-01-22 11:22:36 +0100

    Add some shoppingcart tests

---
+++ cypress/e2e/TheaterLaak/ShoppingCart.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+
+
+
+describe('Visit ShoppingCart and Press \'Verder Winkelen\'', () => { 
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/winkelwagen');
+  });
+  
+  
+
+  it('Visit ShoppingCart and Press \'Verder Winkelen\'', () => {
+    
+    cy.contains('Verder Winkelen').click();
+    
+    cy.url().should('include', '/programmering')
+  });
+
+  it('Empty ShoppingCart Elements', () => {
+    cy.contains("Mijn Winkelwagentje");
+    cy.contains("Er zijn geen items toegevoegd aan het winkelwagentje");
+    cy.contains("Verder Winkelen");
+    cy.contains("Bestellen").should('be.disabled');
+    cy.contains("Totaal:").should('be.hidden')
+  });
+})
+
